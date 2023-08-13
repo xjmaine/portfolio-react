bb7bfc6da5f8783bf2d1701e40af4bbb3efaf878
xjmaine <xjfreddie@gmail.com>
2023-08-13 15:22:41 +0100

    react portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {FaGithub} from 'react-icons/fa'
+import {BsClipboard2Data} from 'react-icons/bs'
 
 const Contact = () => {
   return (
@@ -11,29 +13,34 @@ const Contact = () => {
 
         <div className="container contact__container">
           <div className="contact__options">
-            <article className="contact_option">
-              <MdOutlineEmail/>
+            <article className="contact__option">
+              <MdOutlineEmail className='contact__option-icon'/>
               <h4>Email</h4>
               <h5>remnant23@duck.com</h5>
-              <a href="mailto:remnant23@duck.com">Send a message</a>
+              <a href="mailto:remnant23@duck.com" target='_blank'>Send a message</a>
             </article>
 
-            <article className="contact_option">
-              <MdOutlineEmail/>
-              <h4>GitHud</h4>
-              <h5>remnant23@duck.com</h5>
-              <a href="mailto:remnant23@duck.com">Send a message</a>
+            <article className="contact__option">
+              <FaGithub className='contact__option-icon'/>
+              <h4>GitHub</h4>
+              <h5>View My Profile</h5>
+              <a href="https://github.com/xjmaine/" target='_blank'>Click here</a>
             </article>
 
-            <article className="contact_option">
-              <MdOutlineEmail/>
+            <article className="contact__option">
+              <BsClipboard2Data className='contact__option-icon'/>
               <h4>Data Science Journey</h4>
               <h5>View on ALX</h5>
-              <a href="#">Link</a>
+              <a href="#" target='_blank'>Click here</a>
             </article>
           </div>
           {/* end of contact options */}
-          <form action=""></form>
+          <form action="">
+            <input type="text" name='name' placeholder='Your full name' required/>
+            <input type="email" name='email' placeholder='Your email' required />
+            <textarea name="message"  rows="7" placeholder='Your Message' required></textarea>
+            <button type="submit" className='btn btn-primary'>Send Message</button>
+          </form>
         </div>
       </section>
     </div>
