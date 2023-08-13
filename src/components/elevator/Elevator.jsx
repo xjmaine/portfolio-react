ae24f915cd3b5f5f9c8abdc5f48a3cf4bd2c2793
xjmaine <xjfreddie@gmail.com>
2023-08-13 04:20:43 +0100

    react portfolio

--- src/components/elevator/Elevator.jsx
+++ src/components/elevator/Elevator.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import './elevator.css'
+import { BiCheck } from 'react-icons/bi'
+
 const Elevator = () => {
   return (
     <div>
       <sectopn id='elevator'>
-        
+        <h5>Elevator Pitch</h5>
+        <h2>show case</h2>
+
+        <div className="container elevator__container">
+          <article className='service'>
+            <div className="elevator__head">
+              <h3>Pitch 1</h3>
+            </div>
+            <ul className='elevator__list'>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+            </ul>
+          </article>
+          {/* End of card */}
+
+          <article className='service'>
+            <div className="elevator__head">
+              <h3>Pitch 2</h3>
+            </div>
+            <ul className='elevator__list'>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+            </ul>
+          </article>
+          {/* End of card */}
+
+          <article className='service'>
+            <div className="elevator__head">
+              <h3>Pitch 3</h3>
+            </div>
+            <ul className='elevator__list'>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+              <li>
+                <BiCheck className='elevator__list-icon'/>
+                <p>Empty Pitch Link</p>
+              </li>
+            </ul>
+          </article>
+          {/* End of card */}
+
+        </div>
       </sectopn>
     </div>
   )
